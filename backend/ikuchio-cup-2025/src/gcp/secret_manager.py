bfb6088bac117e2018811b26501d599709fa583b
kurazuuuuuu <110393398+kurazuuuuuu@users.noreply.github.com>
2025-09-14 16:22:53 +0900

    feat: google secret manager

---
+++ backend/ikuchio-cup-2025/src/gcp/secret_manager.py
@@ -0,0 +1,11 @@
+from google.cloud import secretmanager
+
+class SecretManagerUtil:
+    def get_secret(self, project_id: str, secret_id: str) -> str:
+        client = secretmanager.SecretManagerServiceClient()
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+        response = client.access_secret_version(request={"name": name})
+        return response.payload.data.decode("UTF-8")
+
+if __name__ == '__main__':
+    SecretManagerUtil().get_secret("88236233617", "google-vertexai-api-key")
